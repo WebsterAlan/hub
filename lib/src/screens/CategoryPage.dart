ccd6b1eda37ca181b40cfd92f8e89b713a66f4a9
webster <inbox.websense@gmail.com>
2020-01-08 17:29:12 -0300

    Screens CategoryPage,GaleryPage e LocatePage

---
+++ lib/src/screens/CategoryPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class CategoryPage extends StatefulWidget{
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class CategoryPageState extends State<CategoryPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
---
+++ lib/src/screens/LocatePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class LocatePage extends StatefulWidget{
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class LocatePageState extends State<LocatePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
