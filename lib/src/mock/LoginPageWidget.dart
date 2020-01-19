263e8432e36752a0740ebe45edfd31d6111e0ba6
webster <inbox.websense@gmail.com>
2020-01-19 20:53:19 -0300

    Refatoração de novo design de login e registro

---
+++ lib/src/mock/LoginPageWidget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:hub/src/ctrl/ProviderPageController.dart';
+import 'package:hub/src/widgets/CircleButton.dart';
+
+class LoginPageWidget extends StatefulWidget {
+  @override
+  _LoginPageWidgetState createState() => _LoginPageWidgetState();
+}
+
+class _LoginPageWidgetState extends State<LoginPageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      color: Theme.of(context).accentColor.withOpacity(0.9),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            height: 60,
+          ),
+          Icon(
+            Icons.developer_mode,
+            size: 40,
+            color: Colors.white,
+          ),
+          Container(
+            height: 25,
+          ),
+          
+          Container(
+            height: 120,
+          ),
+          CicleButton(
+            backgroundColor: Colors.transparent,
+            bordeColor: Colors.white,
+            label: "CADASTRE-SE",
+            onTap: () {
+              ProviderPageController.of(context).toPage(0);
+            },
+          ),
+          Container(
+            height: 30,
+          ),
+          CicleButton(
+            backgroundColor: Colors.white,
+            textColor: Theme.of(context).accentColor,
+            label: "LOGIN",
+            onTap: () {
+              ProviderPageController.of(context).toPage(2);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/mock/LoginSignInPageWidget.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:hub/src/widgets/CircleButton.dart';
+import 'package:hub/src/widgets/CustomTextField.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginSignInPageWidget extends StatefulWidget {
+  @override
+  _LoginSignInPageWidgetState createState() => _LoginSignInPageWidgetState();
+}
+
+class _LoginSignInPageWidgetState extends State<LoginSignInPageWidget> {
+  Widget _textForgot() {
+    return Container(
+      width: double.infinity,
+      child: Text(
+        "Esqueceu a Senha?",
+        textAlign: TextAlign.right,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _lineTextLine() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Divider(
+          color: Colors.black,
+          height: 15,
+        )),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "CONECTAR COM ",
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Expanded(
+            child: Divider(
+          color: Colors.black,
+          height: 15,
+        )),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white.withOpacity(0.9),
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(height: 80),
+          Icon(Icons.developer_mode,
+              size: 50, color: Theme.of(context).accentColor),
+          Spacer(),
+          CustomTextField(label: "EMAIL", hint: "nome@email.com"),
+          Container(height: 25),
+          CustomTextField(label: "SENHA", hint: "***************"),
+          Container(height: 25),
+          _textForgot(),
+          Container(height: 25),
+          CicleButton(
+            label: "ENTRAR",
+          ),
+          Container(height: 25),
+          _lineTextLine(),
+          Container(height: 25),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: CicleButton(
+                  backgroundColor: Colors.blue[800],
+                  icon: Icon(
+                    FontAwesomeIcons.facebookF,
+                    color: Colors.white,
+                    size: 14,
+                  ),
+                  label: "FACEBOOK",
+                  
+                ),
+              ),
+              Container(
+                width: 20,
+              ),
+              Expanded(
+                child: CicleButton(
+                  backgroundColor: Colors.red[700],
+                  icon: Icon(
+                    FontAwesomeIcons.google,
+                    color: Colors.white,
+                    size: 14,
+                  ),
+                  label: "GOOGLE",
+                ),
+              ),
+            ],
+          ),
+
+          Container(height: 20),
+
+        ],
+      ),
+    );
+  }
+}
