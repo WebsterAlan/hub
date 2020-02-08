220c9e4f57a90a5b7941689eaff043bccdb8dd42
webster <inbox.websense@gmail.com>
2020-02-08 18:15:07 -0300

    Screen de Geolocalização com coordenadas

--- lib/src/screens/SearchLocation.dart
+++ lib/src/screens/SearchLocation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocation/geolocation.dart';
+import 'package:latlong/latlong.dart';
+
 
 class SearchLocation extends StatefulWidget{
   @override
@@ -10,11 +12,32 @@ class SearchLocation extends StatefulWidget{
 class __SearchlocationState extends State<SearchLocation> {
 
   MapController mapController = new MapController();
+  
 
   getPermission() async {
     final GeolocationResult result = await Geolocation.requestLocationPermission(
-    
-    );
+      permission: const LocationPermission(
+        android: LocationPermissionAndroid.fine,
+        ios: LocationPermissionIOS.always));
+        return result;
+  }
+
+  getLocation(){
+    return getPermission().then((result) async {
+      if(result.isSucessful){
+        final coords = await Geolocation.currentLocation(accuracy: LocationAccuracy.best);
+      }
+    });
+  }
+   buildMap(){
+       getLocation().then((response){
+          if(response.isSucessful){
+              response.listen((value){
+               mapController.move(new LatLng(value.location.latitude, value.location.longitude),15.0);
+                 
+              });
+            }
+         });
   }
 
   @override
@@ -27,15 +50,14 @@ class __SearchlocationState extends State<SearchLocation> {
          center: buildMap(),
          minZoom: 5.0
        ),
-       layers: new TileLayerOptions(
+       layers: [
+         new TileLayerOptions(
          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
          subdomains: ['a','b','c']
        ),
-       ),
+       ]),
 
     );
   }
-
-
 }
 
--- lib/src/screens/Splash.dart
+++ lib/src/screens/Splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hub/src/screens/LoginWidget.dart';
+import 'package:hub/src/screens/SearchLocation.dart';
 
 class Splash extends  StatefulWidget {
   
@@ -16,7 +17,7 @@ class _SplashState extends State<Splash> {
     super.initState();
     SystemChrome.setEnabledSystemUIOverlays([]);
     Future.delayed(Duration(seconds: 4)).then((_){
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginWidget()));
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SearchLocation()));
     }); 
   }
   
